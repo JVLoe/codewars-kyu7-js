403f371924bf4b2501122561c7f78e0323726680
Johanna Loe <108595341+JVLoe@users.noreply.github.com>
2023-04-28 11:04:59 +0100

    solution to: Beginner Series #3 Sum of Numbers

--- script.js
+++ script.js
@@ -62,3 +62,36 @@ function getCount(str) {
 function getCount1(str) {
     return (str.match(/[aeiou]/ig)||[]).length;
 }
+
+// Beginner Series #3 Sum of Numbers
+const GetSum = (a, b) => {
+    let min = Math.min(a, b),
+        max = Math.max(a, b);
+    return (max - min + 1) * (min + max) / 2;
+}
+
+function GetSum1(a,b)
+{
+    return (Math.abs(a - b) + 1) * (a+b) / 2;
+}
+
+function GetSum2( a,b )
+{
+//if both a and b are equal return a
+    if(a===b){
+        return a;
+    }
+    var result = 0;
+
+    var x = a, y = b;
+    // check the larger and smaller numbers and assign them to x and y
+    if(a > b) {
+        x = b, y = a;
+    }
+    //in a loop add the numbers from the smaller one until it reaches the larger number
+    for(var i = x; i <=y; i++ ) {
+        result += i;
+    }
+    return result;
+
+}
